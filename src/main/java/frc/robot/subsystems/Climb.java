0698fca445350df0f73230a84ac3e6d64ac5356c
afek <gilad.cohenb@gmail.com>
2025-03-05 14:57:06 +0200

    added scuffed climb

---
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.Percent;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Climb extends SubsystemBase {
+  TalonFX rightTalonFX;
+  TalonFX leftTalonFX;
+  /** Creates a new Climb. */
+  public Climb() {
+    rightTalonFX = new TalonFX(45);
+    leftTalonFX = new TalonFX(46);
+  }
+
+  public void setPercent(double percent){
+    rightTalonFX.set(percent);
+    leftTalonFX.set(percent);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
