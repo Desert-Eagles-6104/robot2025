5795827e9a4f422488937d653e81273c892caf74
afek <gilad.cohenb@gmail.com>
2025-03-22 23:32:01 +0200

    fix file problem with auto

--- src/main/java/frc/robot/Commands/integrationCommands/L3Score.java
+++ src/main/java/frc/robot/Commands/integrationCommands/L3Score.java
@@ -8,9 +8,8 @@ import java.util.function.BooleanSupplier;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.presetState.PresetState;
 import frc.robot.Constants;
-import frc.robot.Commands.ElevatorCommands.ElevatorSetPosition;
+import frc.robot.presetState.PresetState;
 import frc.robot.Commands.GripperArmCommands.DisableGripperArm;
 import frc.robot.Commands.IntakeCommands.IntakeForTime;
 import frc.robot.subsystems.ElevatorSubsystem;
@@ -21,9 +20,9 @@ import frc.robot.subsystems.GripperSubsystem;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class L4Score extends SequentialCommandGroup {
-  /** Creates a new L4Score. */
-  public L4Score(ElevatorSubsystem m_elevator , GripperArmSubsystem m_gripperArm, GripperSubsystem m_gripper ,PresetState m_state,Gripper2Subsystem m_gripper2, BooleanSupplier approve) {
-    addCommands((new SmartPreset(m_elevator, m_gripperArm, m_gripper, m_state.L4)),(new WaitCommand(0.3)),(new IntakeForTime(m_gripper, -0.6, Constants.Intake.TimeToDropIntegraion, approve)),(new WaitCommand(0.3)),new SmartPreset(m_elevator, m_gripperArm, m_gripper, m_state.ZERO),(new DisableGripperArm(m_gripperArm)));
+public class L3Score extends SequentialCommandGroup {
+  /** Creates a new L3Score. */
+  public L3Score(ElevatorSubsystem m_elevator , GripperArmSubsystem m_gripperArm, GripperSubsystem m_gripper ,PresetState m_state,Gripper2Subsystem m_gripper2, BooleanSupplier approve) {
+  addCommands((new SmartPreset(m_elevator, m_gripperArm, m_gripper, m_state.L3)),(new WaitCommand(0.3)),(new IntakeForTime(m_gripper, -0.6, Constants.Intake.TimeToDropIntegraion, approve)),(new WaitCommand(0.3)),new SmartPreset(m_elevator, m_gripperArm, m_gripper, m_state.ZERO),(new DisableGripperArm(m_gripperArm)));
   }
 }
