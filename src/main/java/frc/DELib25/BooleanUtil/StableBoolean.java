c676dd0727c8eff34330928bdb8e531920f91dec
kalmanmoshe <moshe196713@gmail.com>
2025-07-10 00:42:22 +0300

    Refactor StableBoolean to use 'this' keyword for improved clarity and consistency

--- src/main/java/frc/DELib25/BooleanUtil/StableBoolean.java
+++ src/main/java/frc/DELib25/BooleanUtil/StableBoolean.java
@@ -2,6 +2,10 @@ package frc.DELib25.BooleanUtil;
 
 import edu.wpi.first.wpilibj.Timer;
 
+/**
+ * A stable boolean that returns true only if the input has remained true
+ * for at least the specified time threshold.
+ */
 public class StableBoolean {
     private final double timeThreshold;
     private Timer timer;
@@ -9,19 +13,19 @@ public class StableBoolean {
 
     public StableBoolean(double timeThreshold){
         this.timeThreshold = timeThreshold;
-        timer = new Timer();
-        timer.start();
+        this.timer = new Timer();
+        this.timer.start();
     }
 
     public boolean get(boolean input){
-        if(!previousValue && input){
-            timer.reset();
+        if(!this.previousValue && input){
+            this.timer.reset();
         }
-        previousValue = input;
-        return input && timer.hasElapsed(timeThreshold);
+        this.previousValue = input;
+        return input && this.timer.hasElapsed(this.timeThreshold);
     }
 
     public void reset(){
-        timer.reset();
+        this.timer.reset();
     }
 }
