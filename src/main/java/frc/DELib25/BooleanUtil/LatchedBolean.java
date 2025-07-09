1859cd34c86ab16b8c8fe661141116878e8f8224
kalmanmoshe <moshe196713@gmail.com>
2025-07-10 00:22:27 +0300

    Refactor LatchedBolean class to improve variable naming and add Javadoc

--- src/main/java/frc/DELib25/BooleanUtil/LatchedBolean.java
+++ src/main/java/frc/DELib25/BooleanUtil/LatchedBolean.java
@@ -1,19 +1,23 @@
 package frc.DELib25.BooleanUtil;
 
+/**
+ * A latched boolean.
+ * Once set to true, it remains so until reset.
+ */
 public class LatchedBolean {
-    private boolean ret = false;
+    private boolean isLatched = false;
 
     public void update(boolean newValue) {
         if (newValue) {
-            ret = true;
+            this.isLatched = true;
         }
     }
 
     public void reset(){
-        ret = false;
+        this.isLatched = false;
     }
 
     public boolean get(){
-        return ret;
+        return this.isLatched;
     }
 }
