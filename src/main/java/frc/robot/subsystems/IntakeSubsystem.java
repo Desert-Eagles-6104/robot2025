9e434a581c67c3d8c6a3b1c696def8a35a8953bb
kalmanmoshe <moshe196713@gmail.com>
2025-07-22 20:30:07 +0300

    Refactor: Improve IntakeSubsystem structure and variable naming

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -11,7 +11,6 @@ import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
@@ -19,53 +18,58 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class IntakeSubsystem extends SubsystemBase {
-  private static IntakeSubsystem m_instance = null;
 
-  private TalonFX m_motor;
- 
-  
-  private PositionVoltage m_PositionVoltageRequest = new PositionVoltage(0);
+  private TalonFX motor;
 
   private TalonFXConfiguration configuration;
 
-  private StatusSignal<Angle> m_positionSignal;
-  private StatusSignal<AngularVelocity> m_velocitySignal;
-  private StatusSignal<Double> m_closedLoopErrorSignal;
+  private StatusSignal<Angle> positionSignal;
+  private StatusSignal<AngularVelocity> velocitySignal;
+  private StatusSignal<Double> closedLoopErrorSignal;
 
-  public IntakeSubsystem() {
-    configuration = new TalonFXConfiguration();
-    configuration.withMotorOutput(new MotorOutputConfigs().withInverted(Constants.Intake.motorInverted).withDutyCycleNeutralDeadband(Constants.Intake.DutyCycleNeutralDeadband));
-    configuration.withSlot0(new Slot0Configs().withKS(Constants.Intake.Ks).withKV(Constants.Intake.Kv).withKA(Constants.Intake.Ka).withKP(Constants.Intake.Kp).withKI(Constants.Intake.Ki).withKD(Constants.Intake.Kd));
-    configuration.withFeedback(new FeedbackConfigs().withSensorToMechanismRatio(Constants.Intake.SensorToMechanismRatio));
-    configuration.withCurrentLimits(new CurrentLimitsConfigs().withSupplyCurrentLimit(Constants.Intake.supplyCurrentLimit).withSupplyCurrentLimitEnable(Constants.Intake.SupplyCurrentLimitEnable));
+	public IntakeSubsystem() {
+		configuration = new TalonFXConfiguration();
 
-    m_motor = new TalonFX(Constants.Intake.motorId);
-    m_motor.getConfigurator().apply(configuration);
+		configuration.withMotorOutput(new MotorOutputConfigs()
+			.withInverted(Constants.Intake.motorInverted)
+			.withDutyCycleNeutralDeadband(Constants.Intake.DutyCycleNeutralDeadband)
+		);
+		
+		configuration.withSlot0(new Slot0Configs()
+			.withKS(Constants.Intake.Ks)
+			.withKV(Constants.Intake.Kv)
+			.withKA(Constants.Intake.Ka)
+			.withKP(Constants.Intake.Kp)
+			.withKI(Constants.Intake.Ki)
+			.withKD(Constants.Intake.Kd)
+		);
 
-    m_positionSignal = m_motor.getPosition();
-    m_velocitySignal = m_motor.getVelocity();
-    m_closedLoopErrorSignal = m_motor.getClosedLoopError();
-    BaseStatusSignal.setUpdateFrequencyForAll(Constants.Intake.frequencyHz ,m_positionSignal, m_velocitySignal, m_closedLoopErrorSignal);
-  }
+		configuration.withFeedback(new FeedbackConfigs()
+			.withSensorToMechanismRatio(Constants.Intake.SensorToMechanismRatio)
+		);
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+		configuration.withCurrentLimits(new CurrentLimitsConfigs()
+			.withSupplyCurrentLimit(Constants.Intake.supplyCurrentLimit)
+			.withSupplyCurrentLimitEnable(Constants.Intake.SupplyCurrentLimitEnable)
+		);
 
-  public void disableMotors(){
-    m_motor.disable();
-  }
+		this.motor = new TalonFX(Constants.Intake.motorId);
+		this.motor.getConfigurator().apply(configuration);
 
-  public void setMotorPercent(double percent){
-    m_motor.set(percent);
-  }
+		this.positionSignal = this.motor.getPosition();
+		this.velocitySignal = this.motor.getVelocity();
+		this.closedLoopErrorSignal = this.motor.getClosedLoopError();
+
+		BaseStatusSignal.setUpdateFrequencyForAll(Constants.Intake.frequencyHz ,this.positionSignal, this.velocitySignal, this.closedLoopErrorSignal);
+	}
 
 
-  public static IntakeSubsystem getInstance() {
-    if(m_instance == null){
-      m_instance = new IntakeSubsystem();
-    }  
-    return m_instance;
-  }
+	public void disableMotors(){
+		this.motor.disable();
+	}
+
+	public void setMotorPercent(double percent){
+		this.motor.set(percent);
+	}
+
 }
