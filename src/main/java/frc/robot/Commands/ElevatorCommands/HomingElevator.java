8b3d77bbaecb692b0ae3386c6e86cda02f07d82b
kalmanmoshe <moshe196713@gmail.com>
2025-07-22 19:37:13 +0300

    Refactor: Update HomingElevator to use MotorSubsystemTalon and improve variable naming for clarity

--- src/main/java/frc/robot/Commands/ElevatorCommands/HomingElevator.java
+++ src/main/java/frc/robot/Commands/ElevatorCommands/HomingElevator.java
@@ -1,50 +1,35 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.Commands.ElevatorCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.DELib25.BooleanUtil.StableBoolean;
-import frc.DELib25.Subsystems.MotorSubsystems.Base.Motor.ServoSubsystemTalon;
+import frc.DELib25.Subsystems.MotorSubsystems.MotorBase.MotorSubsystemTalon;
 
 public class HomingElevator extends Command {
-  /** Creates a new HomingElevator. */
-  private ServoSubsystemTalon m_elevator;
+  private MotorSubsystemTalon elevator;
   private double m_currentThreshold = 0.98; //TODO: need to be in const
   private double m_velocityThreshold = 0.1; //TODO: need to be in const
-  private StableBoolean m_condition;
-  public HomingElevator(ServoSubsystemTalon elevator) {
-    // Use addRequirements() here to declare subsystem dependencies.
+  private StableBoolean condition;
+  public HomingElevator(MotorSubsystemTalon elevator) {
+    this.elevator = elevator;
+    this.condition = new StableBoolean(0.2);
     addRequirements(elevator);
-    m_elevator = elevator;
-    m_condition = new StableBoolean(0.2);
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    // m_elevator.setMotionMagicPosition(0.05);
-    m_elevator.ControlSoftLimit(false);
-    m_elevator.setPrecentOutput(-0.05);
+    this.elevator.ControlSoftLimit(false);
+    this.elevator.setPrecentOutput(-0.05);
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-       //need to be homing speed in constants
-  }
 
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_elevator.resetPosition(0);
-    m_elevator.setPrecentOutput(0.0);
+    this.elevator.resetPosition(0);
+    this.elevator.setPrecentOutput(0.0);
   }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return m_condition.update(m_elevator.getMotorCurrent() > m_currentThreshold && m_elevator.getVelocity() < m_velocityThreshold);
+    return this.condition.update(this.elevator.getMotorCurrent() > m_currentThreshold && this.elevator.getVelocity() < m_velocityThreshold);
   }
 }
