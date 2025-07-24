d54613dcd7adee01636ef19365fe321fa3e7ecd7
kalmanmoshe <moshe196713@gmail.com>
2025-07-24 03:35:38 +0300

    Refactor: Update DriveAssist to use VisionSubsystemRobot2025 and remove deprecated VisionSubsystem references

--- src/main/java/frc/DELib25/Subsystems/Swerve/SwerveUtil/DriveAssist.java
+++ src/main/java/frc/DELib25/Subsystems/Swerve/SwerveUtil/DriveAssist.java
@@ -6,15 +6,17 @@ import edu.wpi.first.math.filter.LinearFilter;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import frc.DELib25.Subsystems.Vision.VisionSubsystem;
+import frc.DELib25.Subsystems.Vision.VisionSubsystemRobot2025;
 import frc.robot.Robot;
 
 public class DriveAssist {    
     private double m_kp;
     private BooleanSupplier m_intakeButton;
     private LinearFilter m_filter;
+    private VisionSubsystemRobot2025 vision;
 
-    public DriveAssist (double kp, BooleanSupplier intakeButton){
+    public DriveAssist(VisionSubsystemRobot2025 vision, double kp, BooleanSupplier intakeButton) {
+        this.vision = vision;
         m_kp = kp;
         m_intakeButton = intakeButton;
         m_filter = LinearFilter.movingAverage(4);
@@ -22,12 +24,12 @@ public class DriveAssist {
 
     public ChassisSpeeds update(ChassisSpeeds chassisSpeeds, Rotation2d robotHeading){
         ChassisSpeeds toReturn = new ChassisSpeeds();
-        if(VisionSubsystem.getTvNote() && m_intakeButton.getAsBoolean()){
+        if(this.vision.getTvNote() && m_intakeButton.getAsBoolean()){
             if(Robot.s_Alliance == Alliance.Red){
-                toReturn = ChassisSpeeds.fromRobotRelativeSpeeds(0, -m_filter.calculate(-VisionSubsystem.getTxNote())*m_kp, 0, robotHeading);
+                toReturn = ChassisSpeeds.fromRobotRelativeSpeeds(0, -m_filter.calculate(-this.vision.getTxNote())*m_kp, 0, robotHeading);
             }
             else{
-                toReturn = ChassisSpeeds.fromRobotRelativeSpeeds(0, m_filter.calculate(-VisionSubsystem.getTxNote())*m_kp, 0, robotHeading);
+                toReturn = ChassisSpeeds.fromRobotRelativeSpeeds(0, m_filter.calculate(-this.vision.getTxNote())*m_kp, 0, robotHeading);
             }
         }
         chassisSpeeds.vxMetersPerSecond = toReturn.vxMetersPerSecond + chassisSpeeds.vxMetersPerSecond;
