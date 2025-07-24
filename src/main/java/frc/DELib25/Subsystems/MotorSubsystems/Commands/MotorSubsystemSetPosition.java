06a9dd9947e775e0d5255eacc2063e8615c5c59a
Kalmanmoshe <moshe196713@gmail.com>
2025-07-24 18:46:26 +0300

    Refactor: Replace MotorSubsystemSetPosition with MotorDoubleSupplierCommand for improved command structure

--- src/main/java/frc/DELib25/Subsystems/MotorSubsystems/Commands/MotorSubsystemSetPosition.java
+++ src/main/java/frc/DELib25/Subsystems/MotorSubsystems/Commands/MotorSubsystemSetPosition.java
@@ -2,29 +2,20 @@ package frc.DELib25.Subsystems.MotorSubsystems.Commands;
 
 import java.util.function.DoubleSupplier;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import frc.DELib25.Subsystems.MotorSubsystems.MotorBase.MotorSubsystemTalon;
 
-public class MotorSubsystemSetPosition extends Command {
+public class MotorSubsystemSetPosition extends MotorDoubleSupplierCommand {
     protected MotorSubsystemTalon subsystemTalon;
     protected DoubleSupplier positionSupplier;
 
     public MotorSubsystemSetPosition(MotorSubsystemTalon motorSubsystemTalon, DoubleSupplier positionSupplier) {
-        this.subsystemTalon = motorSubsystemTalon;
-        this.positionSupplier = positionSupplier;
-        addRequirements(motorSubsystemTalon);
+        super(motorSubsystemTalon, positionSupplier);
     }
 
     @Override
     public void execute() {
         this.subsystemTalon.setPosition(this.positionSupplier.getAsDouble());
     }
-
-    @Override
-    public void end(boolean interrupted) {
-        this.subsystemTalon.disableMotors();
-    }
-
     @Override
     public boolean isFinished() {
         return this.subsystemTalon.isAtSetpoint();
