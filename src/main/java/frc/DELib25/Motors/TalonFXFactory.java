65d8fb73cc182b8ada5b8d90c473be2a1be559f3
kalmanmoshe <moshe196713@gmail.com>
2025-07-31 03:46:14 +0300

    Refactor: Simplify TalonFX configuration methods for better maintainability

--- src/main/java/frc/DELib25/Motors/TalonFXFactory.java
+++ src/main/java/frc/DELib25/Motors/TalonFXFactory.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.DELib25.Motors;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
@@ -12,7 +8,6 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import frc.DELib25.Util.PhoneixUtil;
 
-/** Add your docs here. */
 public class TalonFXFactory {
     public static TalonFX createTalonFX(MotorConstants motorConstants, boolean useDefaultConfig){
         TalonFX talon = createTalonFX(motorConstants.id, motorConstants.bus);
@@ -31,12 +26,10 @@ public class TalonFXFactory {
         PhoneixUtil.checkErrorAndRetry(() -> talon.setControl(new Follower(masterId, opposeMasterDirection)));
         return talon;
     }
-
-    public static TalonFXConfiguration getDefaultConfig() {
+    
+    private static TalonFXConfiguration baseDefaultConfig() {
         TalonFXConfiguration config = new TalonFXConfiguration();
 
-        config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
-        config.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
         config.MotorOutput.DutyCycleNeutralDeadband = 0.01;
         config.MotorOutput.PeakForwardDutyCycle = 1.0;
         config.MotorOutput.PeakReverseDutyCycle = -1.0;
@@ -45,7 +38,6 @@ public class TalonFXFactory {
         config.CurrentLimits.SupplyCurrentLowerLimit = 60.0;
         config.CurrentLimits.SupplyCurrentLowerTime = 0.1;
         config.CurrentLimits.SupplyCurrentLimitEnable = true;
-        config.CurrentLimits.StatorCurrentLimitEnable = false;
 
         config.SoftwareLimitSwitch.ForwardSoftLimitEnable = false;
         config.SoftwareLimitSwitch.ForwardSoftLimitThreshold = 0.0;
@@ -53,36 +45,24 @@ public class TalonFXFactory {
         config.SoftwareLimitSwitch.ReverseSoftLimitThreshold = 0.0;
 
         config.Feedback.SensorToMechanismRatio = 1.0;
-
         config.Audio.BeepOnBoot = true;
 
         return config;
     }
 
-        public static TalonFXConfiguration getDefaultConfig(MotorConstants motorConstants) {
-        TalonFXConfiguration config = new TalonFXConfiguration();
+    public static TalonFXConfiguration getDefaultConfig() {
+        TalonFXConfiguration config = baseDefaultConfig();
+        config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+        config.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+        config.CurrentLimits.StatorCurrentLimitEnable = false;
+        return config;
+    }
 
+    public static TalonFXConfiguration getDefaultConfig(MotorConstants motorConstants) {
+        TalonFXConfiguration config = baseDefaultConfig();
         config.MotorOutput.NeutralMode = MotorConstants.toNeturalMode(motorConstants.isBrake);
         config.MotorOutput.Inverted = MotorConstants.toInvertedType(motorConstants.CounterClockwisePositive);
-        config.MotorOutput.DutyCycleNeutralDeadband = 0.01;
-        config.MotorOutput.PeakForwardDutyCycle = 1.0;
-        config.MotorOutput.PeakReverseDutyCycle = -1.0;
-
-        config.CurrentLimits.SupplyCurrentLimit = 40.0;
-        config.CurrentLimits.SupplyCurrentLowerLimit = 60.0;
-        config.CurrentLimits.SupplyCurrentLowerTime = 0.1;
-        config.CurrentLimits.SupplyCurrentLimitEnable = true;
         config.CurrentLimits.StatorCurrentLimitEnable = true;
-
-        config.SoftwareLimitSwitch.ForwardSoftLimitEnable = false;
-        config.SoftwareLimitSwitch.ForwardSoftLimitThreshold = 0.0;
-        config.SoftwareLimitSwitch.ReverseSoftLimitEnable = false;
-        config.SoftwareLimitSwitch.ReverseSoftLimitThreshold = 0.0;
-
-        config.Feedback.SensorToMechanismRatio = 1.0;
-
-        config.Audio.BeepOnBoot = true;
-
         return config;
     }
 
